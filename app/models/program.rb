c7c44001322aa7d7254a4fc2c938ccdbd149978b
kjordan88 <kristy.jordan88@gmail.com>
2016-02-17 16:36:46 -0500

    builds migrations and models

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,2 +1,4 @@
 class Program < ActiveRecord::Base
+  has_many :user_programs
+  has_many :users, through: :user_programs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :user_programs
+  has_many :programs, through: :user_programs
+  has_many :contents
 end
